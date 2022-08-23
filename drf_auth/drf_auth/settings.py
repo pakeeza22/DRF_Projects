1e14196fa9aea8c6196028f93772cbbb4b4e7327
Pakeeza Ehsan <pakeeza22pk@gmail.com>
2022-08-23 16:43:15 +0500

    Update settings.py

--- drf_auth/drf_auth/settings.py
+++ drf_auth/drf_auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d6j(n1@ljbuv6!&js4z9!!ns5$z01i)hlxrdjrk4g+e&ee^=(('
+SECRET_KEY =  'put your secret key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'drf_practice',
         'HOST': '127.0.0.1',
         'PORT': '3308',
-        'USER': 'root',
+        'USER': 'user',
         'PASSWORD': ''}
 }
 
